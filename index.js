e18a34b011b2e669e9fb0c972d5fb1edab0456e0
Rick Belson <belsonrj@aol.com>
2020-08-19 19:38:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,9 +8,23 @@ class Cat {
 }
 
 class Dog {
-  
+  constructor(name) {
+    this.name = name;
+  }
+  speak() {
+    return `${this.name} says woof!`
+  }
 }
 
 class Bird {
-  
+    constructor(name, gender) {
+    this.name = name;
+    this.gender = gender
+  }
+  speak() {
+    if this.gender = "male"
+    return `It's me! ${this.name}, th parrot!`
+    else
+    return `${this.name} says squawk!`
+  }
 }
